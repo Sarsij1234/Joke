91fd14f498d6aa7f0682155b281a590b78c0e68b
Sarsij Upadhyay <152963531+Sarsij1234@users.noreply.github.com>
2024-07-15 19:48:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-     <div className="bg-green-500 w-full h-80 items-cente ">
+     <div className="bg-green-500 w-full h-auto items-cente ">
       <h1 className="text-black items-center p-5 underline text-2xl font-bold">Joke Generator Using React And Joke API </h1>
       
        <Joke/>
